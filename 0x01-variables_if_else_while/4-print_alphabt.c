64100b117ab3a4bf37cc569eb749e085e53d3799
mahmoud <ma9117442@gmail.com>
2023-07-16 16:53:32 -0700

    the new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@
 
 int main(void)
 {
-	char ch = 'a';
+char ch = 'a';
 while (ch <= 'z')
-	{
-		if (ch == 'e' || ch == 'q')
-			ch++;
-		putchar(ch);
-		ch++;
-	}
-	putchar('\n');
+{
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
