c82745d0ab1e7a232c462b7bc1609b9973823363
mahmoud <ma9117442@gmail.com>
2023-08-09 17:44:00 -0700

    the args

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,29 +10,29 @@
 
 int main(int argc, char **argv)
 {
-        int total, change;
+int total, change;
 
-        if (argc < 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
 
-        change = atoi(argv[1]);
+change = atoi(argv[1]);
 
-        for (total = 0; change > 0; total++)
-        {
-                if (change - 25 >= 0)
-                        change = change - 25;
-                else if (change - 10 >= 0)
-                        change = change - 10;
-                else if (change - 5 >= 0)
-                        change = change - 5;
-                else if (change - 2 >= 0)
-                        change = change - 2;
-                else if (change - 1 >= 0)
-                        change = change - 1;
-        }
-        printf("%d\n", total);
-        return (0);
+for (total = 0; change > 0; total++)
+{
+if (change - 25 >= 0)
+change = change - 25;
+else if (change - 10 >= 0)
+change = change - 10;
+else if (change - 5 >= 0)
+change = change - 5;
+else if (change - 2 >= 0)
+change = change - 2;
+else if (change - 1 >= 0)
+change = change - 1;
+}
+printf("%d\n", total);
+return (0);
 }
