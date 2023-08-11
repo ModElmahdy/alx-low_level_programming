9848ee5347f30e1ce15d4ff2d68b93ae983f143c
mahmoud <ma9117442@gmail.com>
2023-08-10 18:04:31 -0700

    the mollac

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,7 @@
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
- */
+*/
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
@@ -17,4 +17,3 @@ if (ptr == NULL)
 exit(98);
 return (ptr);
 }
-
